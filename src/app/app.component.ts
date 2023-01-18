b0e1fc177ee5aaa8a1ec3636407842b028a15a63
Rushabh9338 <93291574+Rushabh9338@users.noreply.github.com>
2023-01-18 22:12:02 +0530

    Pattern for shapes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,6 @@ export class AppComponent implements OnInit {
   }
 
   test(value: any) {
-    console.log(value);
     if (this.drawLayer) this.Map.removeInteraction(this.drawLayer);
     if (value === 'clear') {
       this.vector.getSource()?.clear();
@@ -63,7 +62,7 @@ export class AppComponent implements OnInit {
       this.vector.setStyle(
         new Style({
           fill: new Fill({
-            color: 'red',
+            color: this.makePattern(),
           }),
           stroke: new Stroke({
             color: 'green',
@@ -88,5 +87,83 @@ export class AppComponent implements OnInit {
       );
       this.Map.addInteraction(this.drawLayer);
     }
+  }
+
+  // makePattern = () => {
+  //   var cnv = document.createElement('canvas');
+  //   var ctx = cnv.getContext('2d');
+  //   cnv.width = 6;
+  //   cnv.height = 6;
+  //   ctx.fillStyle = 'rgb(255, 0, 0)';
+
+  //   for (var i = 0; i < 6; ++i) {
+  //     ctx.fillRect(i, i, 1, 1);
+  //   }
+
+  //   return ctx.createPattern(cnv, 'repeat-x');
+  // };
+
+  // makePattern() {
+  //   var myCanvas = document.createElement('canvas');
+  //   var drawingContext = myCanvas.getContext('2d');
+
+  //   var color1 = '#F2EEB3';
+  //   var color2 = '#FF4C65';
+  //   var numberOfStripes = 100;
+  //   for (var i = 0; i < numberOfStripes * 2; i++) {
+  //     var thickness = 300 / numberOfStripes;
+  //     drawingContext.beginPath();
+  //     drawingContext.strokeStyle = i % 2 ? color1 : color2;
+  //     drawingContext.lineWidth = thickness;
+  //     drawingContext.lineCap = 'round';
+
+  //     // Vertical
+  //     // drawingContext.moveTo(i * thickness + thickness / 2, 0);
+  //     // drawingContext.lineTo(i * thickness + thickness / 2, 300);
+
+  //     // Forward Diagonal
+  //     // drawingContext.moveTo(i * thickness + thickness / 2 - 300, 0);
+  //     // drawingContext.lineTo(0 + i * thickness + thickness / 2, 300);
+
+  //     // Horizontal
+  //     // drawingContext.moveTo(0, i * thickness + thickness / 2);
+  //     // drawingContext.lineTo(300, i * thickness + thickness / 2);
+  //     drawingContext.stroke();
+  //   }
+  //   return drawingContext.createPattern(myCanvas, 'repeat');
+  // }
+
+  //   var myCanvas = document.getElementById(“myCanvas”);
+  // var drawingContext = myCanvas.getContext(“2d”);
+
+  // var color1 = “#24A8AC”,color2=”#0087CB”;
+  // var numberOfStripes = 30;
+  // for (var i=0;i < numberOfStripes;i++){
+  // var thickness = 300 / numberOfStripes;
+  // drawingContext.beginPath();
+  // drawingContext.strokeStyle = i % 2?color1:color2;
+  // drawingContext.lineWidth =thickness;
+
+  // drawingContext.moveTo(0,i*thickness + thickness/2);
+  // drawingContext.lineTo(300,i*thickness + thickness/2);
+  // drawingContext.stroke();
+  // }
+
+  makePattern() {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    for (let i = 10; i < 200; i += 20) {
+      ctx.moveTo(0, i);
+      ctx.lineTo(canvas.width, i);
+      ctx.stroke();
+    }
+    for (let i = 10; i < 400; i += 20) {
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, canvas.width / 2);
+      ctx.setTransform(1, 0.2, 0.8, 1, 0, 0);
+      ctx.stroke();
+    }
+
+    return ctx.createPattern(canvas, 'repeat');
   }
 }
